62d1c7afe2da2b43033b4d16497b3aabb7e024c9
poojasri-elango47 <poojasrielango47@gmail.com>
2025-12-14 21:55:31 +0530

    getting user input

---
+++ Java/Printing_from_input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Printing_from_input{
+   public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       String name=sc.nextLine();
+       String college=sc.nextLine();
+       String dept=sc.nextLine();
+       System.out.println(name+"\n"+college+"\n"+dept);
+   }}
