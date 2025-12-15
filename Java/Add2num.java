999025795977b53538b49be20dfa3289b02fa515
poojasri-elango47 <poojasrielango47@gmail.com>
2025-12-15 21:14:38 +0530

    Add 2 numbers

---
+++ Java/Add2num.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Add2num{
+    public static void main(String []args)
+{
+    int a;
+    int b;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter 2 numbers:");
+    a=sc.nextInt();
+    b=sc.nextInt();
+    int sum=a+b;
+    System.out.println("Sum="+sum);
+
+}}
