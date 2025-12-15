4395cd74a340d45e84bf6a3dfcb107067bc235c4
poojasri-elango47 <poojasrielango47@gmail.com>
2025-12-15 21:31:43 +0530

    Swapping 2 numbers with temp

---
+++ Java/swapnum.java
@@ -0,0 +1,13 @@
+class swapnum{
+    public static void main(String[] args) {
+        int a=1;
+        int b=2;
+        int temp=0;
+        temp=b;
+        b=a;
+        a=temp;
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+
+    }
+}
